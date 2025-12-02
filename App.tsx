25a3b03b6453c169f7523ea51751a634f9b0279b
MoeJoeCodes <myusufjogee@gmail.com>
2025-12-02 22:04:19 +0200

    viewpoint2

--- App.tsx
+++ App.tsx
@@ -1,22 +1,16 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { KeyboardProvider } from "react-native-keyboard-controller";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
 
 import DrawerNavigator from "@/navigation/DrawerNavigator";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
 import { AnimatedBackground } from "@/components/AnimatedBackground";
 
 export default function App() {
+  const insets = useSafeAreaInsets();
   return (
     <ErrorBoundary>
       <SafeAreaProvider>
         <GestureHandlerRootView style={styles.root}>
           <KeyboardProvider>
-            <View style={styles.container}>
+            <View style={[styles.container, { paddingBottom: insets.bottom }]}> 
               <AnimatedBackground />
               <NavigationContainer>
                 <DrawerNavigator />
